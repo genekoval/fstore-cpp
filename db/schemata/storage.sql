8311758925274db2c396b00a1ddc5d8b284cd171
Gene Koval <genya@nova.local>
2019-12-28 21:11:24 -0500

    Add functions for creating buckets and objects

--- db/schemata/storage.sql
+++ db/schemata/storage.sql
@@ -1,32 +1,72 @@
--- postgresql 12.1
-
 DROP SCHEMA IF EXISTS storage CASCADE;
 
 CREATE SCHEMA storage
 
 CREATE TABLE bucket (
     id              uuid PRIMARY KEY,
-    name            varchar(128) NOT NULL,
-    date_created    date NOT NULL
+    name            varchar(128) UNIQUE NOT NULL,
+    date_created    timestamptz DEFAULT NOW()
 )
 
 CREATE TABLE object (
     -- SHA256 checksum of the file contents.
-    hash            char(64) PRIMARY KEY,
+    hash            char(65) PRIMARY KEY,
 
     -- Length of the file contents in bytes.
     len             bigint NOT NULL,
 
     -- The time this object was first added to the database.
-    date_added      date NOT NULL
+    date_added      timestamptz DEFAULT NOW()
 )
 
 CREATE TABLE bucket_object (
     bucket_id       uuid REFERENCES bucket(id) ON DELETE CASCADE,
-    object_id       char(64) REFERENCES object(hash) ON DELETE CASCADE,
-    date_added      date NOT NULL,
+    object_id       char(65) REFERENCES object(hash) ON DELETE CASCADE,
+    date_added      timestamptz DEFAULT NOW(),
 
     PRIMARY KEY (bucket_id, object_id)
 )
 
-; -- END SCHEMA storage
+;
+
+CREATE FUNCTION storage.add_object(
+    bucket_name     varchar(128),
+    obj_hash        char(65)
+) RETURNS void AS $$
+BEGIN
+    INSERT INTO storage.bucket_object (bucket_id, object_id) VALUES (
+        (
+            SELECT id
+            FROM storage.bucket
+            WHERE name = bucket_name
+        ),
+        obj_hash
+    );
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION storage.create_bucket(
+    bucket_id       uuid,
+    bucket_name     varchar(128)
+) RETURNS void AS $$
+BEGIN
+    INSERT INTO storage.bucket (id, name) VALUES (
+        bucket_id,
+        bucket_name
+    );
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION storage.create_object(
+    obj_hash        char(65),
+    obj_len         bigint
+) RETURNS char(65) AS $$
+BEGIN
+    INSERT INTO storage.object (hash, len) VALUES (
+        obj_hash,
+        obj_len
+    );
+
+    RETURN obj_hash;
+END;
+$$ LANGUAGE plpgsql;
