020346de46ab1e6c6e6907dde83113f5c6512ee0
Gene Koval <genya@nova.local>
2020-01-14 23:10:38 -0500

    Generate the checksum of a file

--- src/fstore/object.cpp
+++ src/fstore/object.cpp
@@ -1,35 +1,51 @@
 #include <repo.h>
 
+#include <algorithm>
+#include <cctype>
 #include <commline/commands.h>
-#include <iostream>
-
 #include <cryptopp/files.h>
 #include <cryptopp/hex.h>
 #include <cryptopp/sha.h>
+#include <filesystem>
+#include <iostream>
 #include <service.h>
-
 #include <string>
+
+namespace fs = std::filesystem;
 
 using std::cout;
 using std::endl;
 using std::string;
+using std::transform;
+
+string sha256sum(string& sink, const fs::path& file) {
+    using namespace CryptoPP;
+
+    SHA256 hash;
+
+    FileSource(
+        file.c_str(),
+        true,
+        new HashFilter(
+            hash,
+            new HexEncoder(new StringSink(sink))
+        )
+    );
+
+    transform(sink.begin(), sink.end(), sink.begin(),
+        [](unsigned char c) -> unsigned char { return tolower(c); });
+
+    return sink;
+}
 
 void commline::commands::add(const commline::cli& cli) {
-        using namespace CryptoPP;
+    if (cli.args().empty())
+        throw commline::cli_error("no filename given");
 
-        HexEncoder encoder(new FileSink(cout));
+    auto file = cli.args().front();
+    string checksum;
 
-        auto message = cli.args().front();
-        string digest;
+    sha256sum(checksum, file);
 
-        SHA256 hash;
-        hash.Update((const byte*) message.data(), message.size());
-        digest.resize(hash.DigestSize());
-        hash.Final((byte*) &digest[0]);
-
-        cout << "Message: " << message << endl;
-
-        cout << "Digest: ";
-        StringSource(digest, true, new Redirector(encoder));
-        cout << endl;
+    cout << checksum << "  " << file << endl;
 }
