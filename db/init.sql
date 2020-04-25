23ba7850849625fde42a45489d0fe00ef6741949
Gene Koval <genya@gaia.local>
2020-04-25 01:46:06 -0400

    Move database initialization to makefile

--- db/init.sql
+++
@@ -1,5 +0,0 @@
--- postgresql 12.1
-
-CREATE USER fstore;
-
-CREATE DATABASE fstore WITH OWNER fstore;
--- mk/db.mk
+++ mk/db.mk
@@ -4,26 +4,41 @@ sql.schemata.files := $(wildcard $(sql.schemata)/*.sql)
 sql.schemata.bases := $(subst $(sql.schemata)/,,$(sql.schemata.files))
 sql.schemata.items := $(subst .sql,,$(sql.schemata.bases))
 
-db.client := psql
-db.project := $(db.client) --username=$(project) --dbname=$(project)
-db.init := $(sql)/init.sql
-define db.run
- $(db.project) --file=$(1)
+db.username := $(project)
+db.dbname := $(project)
+
+db.connect := psql --username=$(db.username) --dbname=$(db.dbname)
+db.command := $(db.connect) --quiet --command
+
+define db.exec
+ $(db.connect) --file=$(1)
 endef
+
+db.superuser ?= postgres
 
 define sql.schemata.item
  schema.$(1):
-	$(call db.run,$(sql.schemata)/$(1).sql)
+	$(call db.exec,$(sql.schemata)/$(1).sql)
 endef
 
 $(foreach item,$(sql.schemata.items),$(eval $(call sql.schemata.item,$(item))))
 
-.PHONY: db schemata
+.PHONY: db
 
 db:
-	@$(db.project)
+	@$(db.connect) --quiet
 
-db.init:
-	$(db.client) --username=DB_SUPERUSER --file=$(db.init)
+db.create:
+	@echo Initializing database...
+	@createuser --username=$(db.superuser) $(db.username)
+	@createdb --username=$(db.superuser) --owner=$(db.username) $(db.dbname)
+	@$(db.command) "ALTER DATABASE $(db.dbname) SET search_path = storage"
+	@echo Created database: $(db.dbname)
 
-schemata: $(addprefix schema.,$(sql.schemata.items))
+db.drop:
+	@echo Dropping database...
+	@dropdb --username=$(db.superuser) --if-exists $(db.dbname)
+	@dropuser --username=$(db.superuser) --if-exists $(db.username)
+	@echo Removed database: $(db.dbname)
+
+db.schema: $(addprefix schema.,$(sql.schemata.items))
