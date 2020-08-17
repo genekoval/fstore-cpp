29bd7f512eb0821c636d7a892297d96efa9660f1
Gene Koval <genya@nova.local>
2020-08-16 23:11:56 -0400

    Avoid reading values based on function arguments
    Due to the order of execution in C++, the calls to `read<Arg>()` in
    `protocol.use` would be executed in a random order. In practice, they
    would be called in reverse. This means the values for function
    parameters would be read in from the client in reverse order.
    
    The fix for this is to simply read the values manually by interacting
    directly with the protocol object.

--- include/fstore/server/server.h
+++ include/fstore/server/server.h
@@ -24,24 +24,16 @@ namespace fstore::server {
         auto send(const void* buffer, std::size_t len) const -> void;
     };
 
-    class protocol : public zipline::protocol<connection> {
-    public:
-        using zipline::protocol<connection>::protocol;
+    struct protocol : zipline::protocol<netcore::socket> {
+        service::object_store* store;
 
-        template <typename R, typename ...Args>
-        auto use(R (*callable)(service::object_store&, Args...)) -> void {
-            const auto result = callable(*(sock->store), read<Args>()...);
-
-            write(true);
-            write(result);
-        }
-
-        template <typename ...Args>
-        auto use(void (*callable)(service::object_store&, Args...)) -> void {
-            callable(*(sock->store), read<Args>()...);
-
-            write(true);
-        }
+        protocol(
+            const netcore::socket& sock,
+            service::object_store& store
+        ) :
+            zipline::protocol<netcore::socket>(sock),
+            store(&store)
+        {}
     };
 
     auto listen(
--- src/libserver/endpoints/endpoints.cpp
+++ src/libserver/endpoints/endpoints.cpp
@@ -3,74 +3,22 @@
 #include <fstore/server/transfer.h>
 
 namespace fstore::server::endpoint {
-    auto $add_object(
-        service::object_store& store,
-        std::string bucket_id,
-        std::string path
-    ) -> model::object {
-        return store.add_object(bucket_id, path);
-    }
-
-    auto $create_bucket(
-        service::object_store& store,
-        std::string name
-    ) -> model::bucket {
-        return store.create_bucket(name);
-    }
-
-    auto $fetch_bucket(
-        service::object_store& store,
-        std::string name
-    ) -> model::bucket {
-        return store.fetch_bucket(name);
-    }
-
-    auto $remove_bucket(
-        service::object_store& store,
-        std::string name
-    ) -> void {
-        store.remove_bucket(name);
-    }
-
-    auto $remove_object(
-        service::object_store& store,
-        std::string bucket_id,
-        std::string object_id
-    ) -> model::object {
-        return store.remove_object(bucket_id, object_id);
-    }
-
-    auto $rename_bucket(
-        service::object_store& store,
-        std::string bucket_id,
-        std::string bucket_name
-    ) -> void {
-        store.rename_bucket(bucket_id, bucket_name);
-    }
-
-// -----------------------------------------------------------------------------
-
     auto add_object(protocol& proto) -> void {
-        proto.use($add_object);
-    }
+        auto bucket_id = proto.read<std::string>();
+        auto path = proto.read<std::string>();
 
-    auto create_bucket(protocol& proto) -> void {
-        proto.use($create_bucket);
+        proto.reply(proto.store->add_object(bucket_id, path));
     }
 
     auto fetch_bucket(protocol& proto) -> void {
-        proto.use($fetch_bucket);
-    }
-
-    auto remove_bucket(protocol& proto) -> void {
-        proto.use($remove_bucket);
+        auto name = proto.read<std::string>();
+        proto.reply(proto.store->fetch_bucket(name));
     }
 
     auto remove_object(protocol& proto) -> void {
-        proto.use($remove_object);
-    }
+        auto bucket_id = proto.read<std::string>();
+        auto object_id = proto.read<std::string>();
 
-    auto rename_bucket(protocol& proto) -> void {
-        proto.use($rename_bucket);
+        proto.reply(proto.store->remove_object(bucket_id, object_id));
     }
 }
