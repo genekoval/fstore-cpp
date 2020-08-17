8ac1c314f125c6241b1b55623575561caa15bd01
Gene Koval <genya@nova.local>
2020-08-16 23:20:31 -0400

    Create a new connection for each API call

--- include/fstore/client.h
+++ include/fstore/client.h
@@ -5,19 +5,19 @@
 
 namespace fstore {
     class object_store {
-        using protocol = zipline::protocol<netcore::socket>;
-
         enum class event : server::event_t {
             add_object,
-            create_bucket,
             fetch_bucket,
-            remove_bucket,
             remove_object,
-            rename_bucket
         };
 
-        const protocol::socket_type sock;
-        zipline::client<protocol, event> client;
+        using protocol = zipline::protocol<netcore::socket>;
+        using client = zipline::client<protocol, event>;
+
+        const std::string endpoint;
+        netcore::socket socket;
+
+        auto connect() -> client;
     public:
         object_store(std::string_view path);
 
@@ -37,9 +37,9 @@ namespace fstore {
     class bucket {
         const std::string id;
         object_store* store;
-
-        bucket(std::string_view id, object_store& store);
     public:
+        bucket(std::string_view id, object_store& store);
+
         auto add(std::string_view path) -> model::object;
 
         auto remove(std::string_view object_id) -> model::object;
--- src/libfstore/object_store.cpp
+++ src/libfstore/object_store.cpp
@@ -5,23 +5,27 @@
 
 namespace fstore {
     object_store::object_store(std::string_view path) :
-        sock(netcore::connect(path)),
-        client(sock)
+        endpoint(path)
     {}
 
     auto object_store::add_object(
         std::string_view bucket_id,
         std::string_view path
     ) -> model::object {
-        return client.send<model::object>(
+        return connect().send<model::object>(
             event::add_object,
             bucket_id,
             path
         );
     }
 
+    auto object_store::connect() -> client {
+        socket = netcore::connect(endpoint);
+        return client(socket);
+    }
+
     auto object_store::fetch_bucket(std::string_view name) -> model::bucket {
-        return client.send<model::bucket>(
+        return connect().send<model::bucket>(
             event::fetch_bucket,
             name
         );
@@ -31,7 +35,7 @@ namespace fstore {
         std::string_view bucket_id,
         std::string_view object_id
     ) -> model::object {
-        return client.send<model::object>(
+        return connect().send<model::object>(
             event::remove_object,
             bucket_id,
             object_id
