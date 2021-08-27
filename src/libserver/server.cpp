95ef22b605bda54efc73cdab8d44acb025cdcafa
Gene Koval <genyaremchuk@protonmail.com>
2021-08-27 04:29:06 -0400

    Move router code to server translation unit

--- src/libserver/server.cpp
+++ src/libserver/server.cpp
@@ -1,9 +1,34 @@
-#include "router/router.h"
+#include "context/context.h"
 
 #include <fstore/net/zipline/transfer.h>
 #include <fstore/server/server.h>
 
 #include <timber/timber>
+
+using fstore::server::context;
+
+namespace {
+    template <typename ...Routes>
+    using router = zipline::router<
+        fstore::net::socket,
+        fstore::net::event_t,
+        fstore::net::error_list,
+        context,
+        Routes...
+    >;
+
+    auto make_router(context& ctx) {
+        return router(
+            ctx,
+            &context::add_object,
+            &context::create_object_from_file,
+            &context::fetch_bucket,
+            &context::get_object,
+            &context::get_object_metadata,
+            &context::remove_object
+        );
+    }
+}
 
 namespace fstore::server {
     auto listen(
@@ -12,11 +37,11 @@ namespace fstore::server {
         const std::function<void()>& callback
     ) -> void {
         auto ctx = context(store);
-        auto router = make_router(ctx);
+        auto routes = make_router(ctx);
 
-        auto server = netcore::server([&router](netcore::socket&& sock) {
+        auto server = netcore::server([&routes](netcore::socket&& sock) {
             auto socket = net::socket(std::move(sock));
-            router.route(socket);
+            routes.route(socket);
         });
 
         server.listen(unix_socket, callback);
