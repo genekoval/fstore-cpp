6f48a175abf9092e2219ad9149930114cf3ca68c
Gene Koval <genyaremchuk@protonmail.com>
2023-10-09 16:58:51 -0400

    Use http::client::request's new 'send' method

--- src/libfstore/sync/http/object_store.cpp
+++ src/libfstore/sync/http/object_store.cpp
@@ -7,7 +7,7 @@ namespace fstore::sync::http {
         const UUID::uuid& bucket_id,
         const std::filesystem::path& path
     ) -> object {
-        return client.add_object(bucket_id, path).json<object>();
+        return client.add_object(bucket_id, path).send<object>();
     }
 
     auto object_store::download_object(
@@ -19,7 +19,7 @@ namespace fstore::sync::http {
     }
 
     auto object_store::fetch_bucket(std::string_view name) -> bucket {
-        return client.fetch_bucket(name).json<bucket>();
+        return client.fetch_bucket(name).send<bucket>();
     }
 
     auto object_store::get_object(
@@ -41,24 +41,24 @@ namespace fstore::sync::http {
         const UUID::uuid& bucket_id,
         const UUID::uuid& object_id
     ) -> object {
-        return client.get_object_metadata(bucket_id, object_id).json<object>();
+        return client.get_object_metadata(bucket_id, object_id).send<object>();
     }
 
     auto object_store::get_server_info() -> server_info {
-        return client.get_server_info().json<server_info>();
+        return client.get_server_info().send<server_info>();
     }
 
     auto object_store::remove_object(
         const UUID::uuid& bucket_id,
         const UUID::uuid& object_id
     ) -> object {
-        return client.remove_object(bucket_id, object_id).json<object>();
+        return client.remove_object(bucket_id, object_id).send<object>();
     }
 
     auto object_store::remove_objects(
         const UUID::uuid& bucket_id,
         std::span<const UUID::uuid> objects
     ) -> remove_result {
-        return client.remove_objects(bucket_id, objects).json<remove_result>();
+        return client.remove_objects(bucket_id, objects).send<remove_result>();
     }
 }
